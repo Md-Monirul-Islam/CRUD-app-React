bdb1aecfe7e72201c8f86a6931fc5c2ace8bbd79
Md-Monirul-Islam <md.monirulmunna.cse@gmail.com>
2024-01-15 22:10:28 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,96 +1,112 @@
-import { Edit, PlusCircle, Trash2 } from 'react-feather';
 import './App.css';
+import React, { useState } from 'react';
 import 'react-responsive-modal/styles.css';
+import { PlusCircle, Edit, Trash2 } from 'react-feather';
 import { Modal } from 'react-responsive-modal';
-import { useState } from 'react';
-
-
 function App() {
 
-  const blankUser = {
-    'name':'',
-    'email':'',
-    'role':'',
-    'address':'',
+  const blankuser = {
+    "name":"",
+    "email":"",
+    "address":""
   }
 
   const [open, setOpen] = useState(false);
+  const [action,setAction] = useState('Add');
+  const [userdata, setUserdata] = useState([]);
+  const [user, setUser] = useState(blankuser);
+  const [editIndex, setEditIndex] = useState(null);
 
-  const [user, setUser] = useState(blankUser);
-
-  const [userData, setUserData] = useState([])
 
   const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
+  const onCloseModal = () => {
+    setOpen(false);
+    setAction('Add')
+  }
 
   const addUser = () => {
-    setUserData([...userData,user])
-    setUser(blankUser)
+    setUserdata([...userdata,user]);
+    setUser(blankuser);
     onCloseModal();
   }
 
+  const editUser = (index) => {
+    console.log("index",index);
+    setAction('Edit');
+    const selectedUser = userdata.find((x,i) => i === index);
+    setUser(selectedUser);
+    setEditIndex(index);
+    onOpenModal(); 
+  }
+
+  const updateUser = () => {
+    const newusers = userdata.map((x,i) => {
+      if(i === editIndex){
+        x = user
+      }
+      return x
+    });
+    setUserdata(newusers);
+    setUser(blankuser);
+    setEditIndex(null);
+    onCloseModal();
+  }
+
+  const deleteUser = (index) => {
+    const newusers = userdata.filter((x,i) => {return i !== index});
+    setUserdata(newusers);
+  }
+
   return (
+    
     <div className="container">
-      <div className='d-flex'>
+      <div className="d-flex">
         <h1>CRUD APP</h1>
       </div>
       <div className="toolbar">
-        <div className="btn" onClick={onOpenModal}>
-          <PlusCircle size={16}></PlusCircle>
-          <span>Add</span>
-        </div>
-      </div><hr />
-
-      <p>{ JSON.stringify(userData) }</p>
+      <button className='btn btn-p' onClick={onOpenModal}><PlusCircle size={16}></PlusCircle><span>Add</span></button>
+      </div>
+      <hr />
+      {/* <p>{JSON.stringify(userdata)}</p> */}
       <table className='table'>
         <thead>
           <tr>
             <th>Name</th>
             <th>Email</th>
-            <th>Role</th>
             <th>Address</th>
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Munna</td>
-            <td>munna@gmail.com</td>
-            <td>CEO</td>
-            <td>SMR</td>
-            <td>
-              <button className='btn ml2'>
-                <Edit size={16}></Edit>
-                <span>Edit</span>
-              </button>
-              <button className='btn ml2'>
-                <Trash2 size={16}></Trash2>
-                <span>Delete</span>
-              </button>
-            </td>
-          </tr>
+          {userdata.length > 0 && userdata.map((user,index) => {
+            return (<tr>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.address}</td>
+              <td>
+                <button className='btn ml2' onClick={() => editUser(index)}><Edit size={16}></Edit><span>Edit</span></button>
+                <button className='btn ml2' onClick={() => deleteUser(index)}><Trash2 size={16}></Trash2><span>Delete</span></button>
+              </td>
+            </tr>)
+          })
+          }
         </tbody>
       </table>
 
       <Modal open={open} onClose={onCloseModal} center>
-        <h2>Add User</h2>
-        <p>{ JSON.stringify(user) }</p>
-        <div className="form">
-            <label htmlFor="name">Name</label>
-            <input type="text" onChange={(e)=> setUser({...user,'name':e.target.value})} />
-
-            <label htmlFor="email">Email</label>
-            <input type="text" onChange={(e)=> setUser({...user,'email':e.target.value})} />
-
-            <label htmlFor="role">Role</label>
-            <input type="text" onChange={(e)=> setUser({...user,'role':e.target.value})} />
-
-            <label htmlFor="address">Address</label>
-            <textarea name="address" id="" cols="30" rows="10"  onChange={(e)=> setUser({...user,'address':e.target.value})}></textarea>
-            <button className='btn' onClick={()=>addUser()}>Submit</button>
+        <h2>{action} User</h2>
+        {/* <p>{JSON.stringify(user)}</p> */}
+        <div className='form'>
+          <label htmlFor="">Name</label>
+          <input type="text" value={user.name} onChange={(e) => setUser({...user,"name":e.target.value})} />
+          <label htmlFor="">Email</label>
+          <input type="text" value={user.email} onChange={(e) => setUser({...user,"email":e.target.value})} />
+          <label htmlFor="">Address</label>
+          <textarea name="" id="" value={user.address} cols="30" rows="5" onChange={(e) => setUser({...user,"address":e.target.value})}></textarea>
+          {action === 'Add' && <button className='btn' onClick={() => addUser()}>Submit</button>}
+          {action === 'Edit' && <button className='btn' onClick={() => updateUser()}>Update</button>}
         </div>
       </Modal>
-
     </div>
   );
 }
